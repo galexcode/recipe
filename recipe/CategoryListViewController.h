ba8957d7ef349a675e9d5e64184136ef4901dc22
Trung <moc@MOC-MAC.local>
2012-06-26 15:27:03 +0700

    add table pull to refresh (form categories)

--- recipe/CategoryListViewController.h
+++ recipe/CategoryListViewController.h
@@ -12,6 +12,16 @@
 @interface CategoryListViewController : UITableViewController
 {
     NSMutableDictionary *_categoryDictionary;
+    
+    UIView *refreshHeaderView;
+    UILabel *refreshLabel;
+    UIImageView *refreshArrow;
+    UIActivityIndicatorView *refreshSpinner;
+    BOOL isDragging;
+    BOOL isLoading;
+    NSString *textPull;
+    NSString *textRelease;
+    NSString *textLoading;
 }
 
 @property (nonatomic) NSMutableDictionary *categoryDictionary;
@@ -20,5 +30,18 @@
 -(void)tapOnHeader:(id)sender;
 -(void)reload;
 -(void) didParsedCategories;
+
+@property (nonatomic, retain) UIView *refreshHeaderView;
+@property (nonatomic, retain) UILabel *refreshLabel;
+@property (nonatomic, retain) UIImageView *refreshArrow;
+@property (nonatomic, retain) UIActivityIndicatorView *refreshSpinner;
+@property (nonatomic, copy) NSString *textPull;
+@property (nonatomic, copy) NSString *textRelease;
+@property (nonatomic, copy) NSString *textLoading;
+
+- (void)setupStrings;
+- (void)addPullToRefreshHeader;
+- (void)startLoading;
+- (void)stopLoading;
 
 @end
--- recipe/CategoryListViewController.m
+++ recipe/CategoryListViewController.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2012 OngSoft. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
 #import "CategoryListViewController.h"
 #import "ASIForm2DataRequest.h"
 #import "ASI2HTTPRequest.h"
@@ -17,6 +18,7 @@
 
 #define kHeadlineSectionHeight  26
 #define kRegularSectionHeight   18
+#define REFRESH_HEADER_HEIGHT 52.0f
 
 @interface CategoryListViewController ()
 
@@ -25,6 +27,7 @@
 @implementation CategoryListViewController
 @synthesize categoryDictionary = _categoryDictionary;
 @synthesize navController;
+@synthesize textPull, textRelease, textLoading, refreshHeaderView, refreshLabel, refreshArrow, refreshSpinner;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -33,6 +36,7 @@
 //        APP_SERVICE(appSrv);
 //        NSLog(@"%@", appSrv);
 //        _applicationService = appSrv;
+        [self setupStrings];
         [self.tableView setBackgroundColor:[UIColor clearColor]];
         self.tableView.rowHeight = kCellHeight + (kRowVerticalPadding * 0.5) + ((kRowVerticalPadding * 0.5) * 0.5);
         
@@ -121,6 +125,7 @@
     }];
     
     [request startAsynchronous];
+    [self performSelector:@selector(stopLoading) withObject:nil afterDelay:0.5];
 }
 
 -(void) didParsedCategories
@@ -143,6 +148,7 @@
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    [self addPullToRefreshHeader];
 }
 
 - (void)viewDidUnload
@@ -189,74 +195,81 @@
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
-    UIView *customSectionHeaderView;
-    UILabel *titleLabel;
-    HeaderButton *headerButton;
-    
-    //Custom Section Header
-    customSectionHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, kRegularSectionHeight)];
+    if ([self.categoryDictionary.allKeys count] > 0) {
+        UIView *customSectionHeaderView;
+        UILabel *titleLabel;
+        HeaderButton *headerButton;
         
-    titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, tableView.frame.size.width, kRegularSectionHeight)];
-    
-    customSectionHeaderView.backgroundColor = [UIColor colorWithRed:0.44f green:0.11f blue:0.05f alpha:1.00f];;
-    
-    titleLabel.textAlignment = UITextAlignmentLeft;
-    [titleLabel setTextColor:[UIColor whiteColor]];
-    [titleLabel setBackgroundColor:[UIColor clearColor]];   
-    titleLabel.font = [UIFont boldSystemFontOfSize:13];
-    
-    NSSortDescriptor* sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:nil ascending:YES selector:@selector(localizedCompare:)];
-    NSArray* sortedCategories = [self.categoryDictionary.allKeys sortedArrayUsingDescriptors:[NSArray arrayWithObject:sortDescriptor]];
-    
-    NSString *categoryName = [sortedCategories objectAtIndex:section];
-    
-    NSArray *currentCategory = [self.categoryDictionary objectForKey:categoryName];
-    
-    Category *thisCategory = (Category*)currentCategory;
-    
-    [titleLabel setText:categoryName];
-    [titleLabel setTextColor:[UIColor colorWithRed:0.76f green:0.54f blue:0.29f alpha:1.00f]];
-    
-    [customSectionHeaderView addSubview:titleLabel];
-    
-    //Add button for header
-    headerButton = [[HeaderButton alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, kRegularSectionHeight)];
-    [headerButton setArray:[thisCategory latestRecipes]];
-    [headerButton setTitleText:categoryName];
-    [headerButton setBackgroundColor:[UIColor clearColor]];
-    [headerButton addTarget:self action:@selector(tapOnHeader:) forControlEvents:UIControlEventTouchUpInside];
-    
-    [customSectionHeaderView addSubview:headerButton];
-    
-    return customSectionHeaderView;
+        //Custom Section Header
+        customSectionHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, kRegularSectionHeight)];
+        
+        titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, tableView.frame.size.width, kRegularSectionHeight)];
+        
+        customSectionHeaderView.backgroundColor = [UIColor colorWithRed:0.44f green:0.11f blue:0.05f alpha:1.00f];;
+        
+        titleLabel.textAlignment = UITextAlignmentLeft;
+        [titleLabel setTextColor:[UIColor whiteColor]];
+        [titleLabel setBackgroundColor:[UIColor clearColor]];   
+        titleLabel.font = [UIFont boldSystemFontOfSize:13];
+        
+        NSSortDescriptor* sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:nil ascending:YES selector:@selector(localizedCompare:)];
+        NSArray* sortedCategories = [self.categoryDictionary.allKeys sortedArrayUsingDescriptors:[NSArray arrayWithObject:sortDescriptor]];
+        
+        NSString *categoryName = [sortedCategories objectAtIndex:section];
+        
+        NSArray *currentCategory = [self.categoryDictionary objectForKey:categoryName];
+        
+        Category *thisCategory = (Category*)currentCategory;
+        
+        [titleLabel setText:categoryName];
+        [titleLabel setTextColor:[UIColor colorWithRed:0.76f green:0.54f blue:0.29f alpha:1.00f]];
+        
+        [customSectionHeaderView addSubview:titleLabel];
+        
+        //Add button for header
+        headerButton = [[HeaderButton alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, kRegularSectionHeight)];
+        [headerButton setArray:[thisCategory latestRecipes]];
+        [headerButton setTitleText:categoryName];
+        [headerButton setBackgroundColor:[UIColor clearColor]];
+        [headerButton addTarget:self action:@selector(tapOnHeader:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [customSectionHeaderView addSubview:headerButton];
+        
+        return customSectionHeaderView;
+    }
+    return nil;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{    
+{
+    
     static NSString *CellIdentifier = @"HorizontalCell";
     
     //HorizontalTableCell *cell = (HorizontalTableCell*)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     HorizontalTableCell *cell;
     
-    if (cell == nil)
-    {
-        cell = [[HorizontalTableCell alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, tableView.frame.size.height)];
-        //cell.navController = self.navigationController;
-        cell.navController = self.navController;
-    }
+    if ([self.categoryDictionary.allKeys count] > 0) {
+        if (cell == nil)
+        {
+            cell = [[HorizontalTableCell alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, tableView.frame.size.height)];
+            cell.navController = self.navController;
+        }
         NSSortDescriptor* sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:nil ascending:YES selector:@selector(localizedCompare:)];
         NSArray* sortedCategories = [self.categoryDictionary.allKeys sortedArrayUsingDescriptors:[NSArray arrayWithObject:sortDescriptor]];
-    
+        
         NSString *categoryName = [sortedCategories objectAtIndex:indexPath.section];
-    
+        
         NSMutableArray *currentCategory = [self.categoryDictionary objectForKey:categoryName];
-    
+        
         Category* thisCategory = (Category *)currentCategory;
         
         NSLog(@"Category: %@ has %d recipes", categoryName, [thisCategory.latestRecipes count]);
-    
+        
         cell.recipes = thisCategory.latestRecipes;
-    
+    } else {
+        cell = [[HorizontalTableCell alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, tableView.frame.size.height)];
+        cell.navController = self.navController;
+    }
     return cell;
 }
 
@@ -299,4 +312,106 @@
 }
 */
 
+- (void)setupStrings{
+    textPull = [[NSString alloc] initWithString:@"Pull down to refresh..."];
+    textRelease = [[NSString alloc] initWithString:@"Release to refresh..."];
+    textLoading = [[NSString alloc] initWithString:@"Loading..."];
+}
+
+- (void)addPullToRefreshHeader {
+    refreshHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0 - REFRESH_HEADER_HEIGHT, 320, REFRESH_HEADER_HEIGHT)];
+    refreshHeaderView.backgroundColor = [UIColor clearColor];
+    
+    refreshLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, REFRESH_HEADER_HEIGHT)];
+    refreshLabel.backgroundColor = [UIColor clearColor];
+    refreshLabel.font = [UIFont systemFontOfSize:18.0];
+    refreshLabel.textAlignment = UITextAlignmentCenter;
+    refreshLabel.textColor = [UIColor colorWithRed:0.76f green:0.54f blue:0.29f alpha:0.75f];
+    
+    refreshArrow = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"arrow_pull_refresh.png"]];
+    refreshArrow.frame = CGRectMake(floorf((REFRESH_HEADER_HEIGHT - 27) / 2),
+                                    (floorf(REFRESH_HEADER_HEIGHT - 44) / 2),
+                                    27, 44);
+    
+    refreshSpinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    refreshSpinner.frame = CGRectMake(floorf(floorf(REFRESH_HEADER_HEIGHT - 20) / 2), floorf((REFRESH_HEADER_HEIGHT - 20) / 2), 20, 20);
+    refreshSpinner.hidesWhenStopped = YES;
+    
+    [refreshHeaderView addSubview:refreshLabel];
+    [refreshHeaderView addSubview:refreshArrow];
+    [refreshHeaderView addSubview:refreshSpinner];
+    [self.tableView addSubview:refreshHeaderView];
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    if (isLoading) return;
+    isDragging = YES;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (isLoading) {
+        // Update the content inset, good for section headers
+        if (scrollView.contentOffset.y > 0)
+            self.tableView.contentInset = UIEdgeInsetsZero;
+        else if (scrollView.contentOffset.y >= -REFRESH_HEADER_HEIGHT)
+            self.tableView.contentInset = UIEdgeInsetsMake(-scrollView.contentOffset.y, 0, 0, 0);
+    } else if (isDragging && scrollView.contentOffset.y < 0) {
+        // Update the arrow direction and label
+        [UIView animateWithDuration:0.25 animations:^{
+            if (scrollView.contentOffset.y < -REFRESH_HEADER_HEIGHT) {
+                // User is scrolling above the header
+                refreshLabel.text = self.textRelease;
+                [refreshArrow layer].transform = CATransform3DMakeRotation(M_PI, 0, 0, 1);
+            } else { 
+                // User is scrolling somewhere within the header
+                refreshLabel.text = self.textPull;
+                [refreshArrow layer].transform = CATransform3DMakeRotation(M_PI * 2, 0, 0, 1);
+            }
+        }];
+    }
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
+    if (isLoading) return;
+    isDragging = NO;
+    if (scrollView.contentOffset.y <= -REFRESH_HEADER_HEIGHT) {
+        // Released above the header
+        [self startLoading];
+    }
+}
+
+- (void)startLoading {
+    isLoading = YES;
+    
+    // Show the header
+    [UIView animateWithDuration:0.3 animations:^{
+        self.tableView.contentInset = UIEdgeInsetsMake(REFRESH_HEADER_HEIGHT, 0, 0, 0);
+        refreshLabel.text = self.textLoading;
+        refreshArrow.hidden = YES;
+        [refreshSpinner startAnimating];
+    }];
+    
+    // Refresh action!
+    [self reload];
+}
+
+- (void)stopLoading {
+    isLoading = NO;
+    
+    // Hide the header
+    [UIView animateWithDuration:0.3 animations:^{
+        self.tableView.contentInset = UIEdgeInsetsZero;
+        [refreshArrow layer].transform = CATransform3DMakeRotation(M_PI * 2, 0, 0, 1);
+    } 
+                     completion:^(BOOL finished) {
+                         [self performSelector:@selector(stopLoadingComplete)];
+                     }];
+}
+
+- (void)stopLoadingComplete {
+    // Reset the header
+    refreshLabel.text = self.textPull;
+    refreshArrow.hidden = NO;
+    [refreshSpinner stopAnimating];
+}
 @end
